70389ea40e65924a4fe1449d132af06ab8ae0b59
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-06-15 20:40:35 +0800

    add logger to kafka listener

--- src/main/java/com/jpmc/midascore/service/KafkaTransactionListener.java
+++ src/main/java/com/jpmc/midascore/service/KafkaTransactionListener.java
@@ -1,16 +1,20 @@
 package com.jpmc.midascore.service;
 import com.jpmc.midascore.foundation.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaTransactionListener {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaTransactionListener.class);
+
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topics = "${general.kafka-topic}")
     public void listen(Transaction transaction){
         try{
-            System.out.println(transaction.getAmount());
+            logger.info("Recieved transaction : {}", transaction.toString());
         } catch (Exception e){
-            System.out.println("Error in deserailising transaction");
+            logger.error("An error occured while deserialising the transaction");
         }
     }
 
